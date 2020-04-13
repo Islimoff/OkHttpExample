93b989bed73523f5da14b88acaa6687f248e0c1e
Ihor Sapronov <islimoff@gmail.com>
2020-04-13 23:17:18 +0300

    first commit

---
+++ app/src/main/java/com/sapronov/okhttpexample/MainActivity.java
@@ -0,0 +1,51 @@
+package com.sapronov.okhttpexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result=findViewById(R.id.result);
+        OkHttpClient client= new OkHttpClient();
+        String url="https://jsonplaceholder.typicode.com/posts/1";
+        final Request request=new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()){
+                    final String strResponse=response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            result.setText(strResponse);
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
